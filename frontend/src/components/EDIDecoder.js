d59dbb629f8075e377d52fe95cd5b6a91e2816af
jhua0940 <jhua0940@gmail.com>
2025-04-14 23:00:06 +1000

    initial

--- frontend/src/components/EDIDecoder.js
+++ frontend/src/components/EDIDecoder.js
@@ -49,7 +49,7 @@ const EDIDecoder = ({ ediMessage, setEdiMessage, decodedItems, setDecodedItems }
       setValidationError('');
       setSuccess(false);
 
-      const response = await fetch('/api/edi/decode', {
+      const response = await fetch('http://3.25.202.61:8000/api/edi/decode', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: ediMessage }),
